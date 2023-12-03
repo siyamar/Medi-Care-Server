777a96d63f23912b599416c1ef06b8265ac199d7
siyamar <ab.rahmansiyam@gmail.com>
2023-12-03 21:45:14 +0600

    added organizer related some api like patch

--- index.js
+++ index.js
@@ -144,12 +144,12 @@ async function run() {
       res.send(result)
 
     })
-    app.patch('/users/supper/:id', async(req, res)=>{
+    app.patch('/users/organizer/:id', async(req, res)=>{
       const id = req.params.id;
       const filter= {_id: new ObjectId(id)};
       const updatedDoc = {
         $set:{
-          role: 'supper'
+          role: 'organizer'
         }
       }
       const result = await userCollection.updateOne(filter, updatedDoc);
